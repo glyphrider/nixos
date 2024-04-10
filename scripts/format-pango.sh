0c6ae2140f0d80188a56485ee5b76baa9ae2efbe
Brian H. Ward <glyphrider@gmail.com>
2024-04-10 08:35:52 -0400

    scripting attempt #3

--- scripts/format-pango.sh
+++ scripts/format-pango.sh
@@ -2,13 +2,13 @@
 
 mkfs.fat -F 32 /dev/nvme0n1p1
 
-mkfs.brtrs /dev/mapper/nixos
+mkfs.btrfs /dev/mapper/nixos
 mount /dev/mapper/nixos /mnt
 for sub in @{,var,nix,home}; do
   btrfs subv create /mnt/$sub
 done
 umount /mnt
-mount -o subvol=@ /dev/mapper/nixos
+mount -o subvol=@ /dev/mapper/nixos /mnt
 
 for sub in {var,nix,home}; do
   mount --mkdir -o subvol=@$sub /dev/mapper/nixos /mnt/$sub
