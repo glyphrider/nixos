73e486e38add8c1ed6f85a9f8a7dc5943e06cafe
Brian H. Ward <glyphrider@gmail.com>
2024-04-22 13:55:07 -0400

    basic layout for vlans in networking setup

--- hosts/beast/network-configuration.nix
+++ hosts/beast/network-configuration.nix
@@ -27,16 +27,29 @@
           MACAddress = "B6:86:EA:F0:DE:2E";
         };
       };
+      "10-vlan3" = {
+        netdevConfig = {
+          Name="vlan3";
+          Kind="vlan";
+        };
+        vlanConfig.Id=3;
+      };
+      "10-vlan113" = {
+        netdevConfig = {
+          Name="vlan113";
+          Kind="vlan";
+        };
+        vlanConfig.Id=113;
+      };
     };
     networks = {
       "20-br0-bind" = {
-        matchConfig.Name = "enp39s0";
+        matchConfig.Name = "enp38s0";
         networkConfig.Bridge = "br0";
       };
       "30-br0-ip" = {
         matchConfig.Name = "br0";
-        # address = [ "192.168.47.252/24" ];
-        # routes = [ { routeConfig.Gateway = "192.168.47.1"; } ];
+        # vlan = [ "vlan3" "vlan113" ];
         networkConfig = {
           DHCP = "ipv4";
           IPForward = "yes";
@@ -46,6 +59,24 @@
           UseDNS = "yes";
           UseDomains = "yes";
         };
+      };
+      "30-vlan3-ip" = {
+        matchConfig.Name="vlan3";
+        networkConfig = {
+          DHCP="ipv4";
+          IPForward="yes";
+          MulticastDNS="yes";
+        };
+        dhcpConfig.UseRoutes=false;
+      };
+      "30-vlan113-ip" = {
+        matchConfig.Name="vlan113";
+        networkConfig = {
+          DHCP="ipv4";
+          IPForward="yes";
+          MulticastDNS="yes";
+        };
+        dhcpConfig.UseRoutes=false;
       };
     };
   };
