c02b21491fa4998cbc0f14cbc2100b47239d4e02
Brian H. Ward <glyphrider@gmail.com>
2024-05-22 07:26:42 -0400

    pango changes (plasma+sway)

--- hosts/pango/configuration.nix
+++ hosts/pango/configuration.nix
@@ -5,5 +5,7 @@
     ./hardware-configuration.nix
     ./network-configuration.nix
     ../optional/amd-graphics-driver-configuration.nix
+    ../optional/plasma-configuration.nix
+    ../optional/sway-configuration.nix
   ];
 }
